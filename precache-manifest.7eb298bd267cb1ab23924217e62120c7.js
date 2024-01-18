3ba6c2ca5b663f9ff53cd2fd0cfc5fad0b94023c
Tanya <tanya777fan@mail.ru>
2024-01-18 15:51:44 +0300

    Updates

---
+++ precache-manifest.7eb298bd267cb1ab23924217e62120c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76204114f4a6d976a4f2d2f4bc34a4f3",
+    "url": "/fetch-jokes/index.html"
+  },
+  {
+    "revision": "ff09c25cafa73ebf904d",
+    "url": "/fetch-jokes/static/css/main.e470ac8e.chunk.css"
+  },
+  {
+    "revision": "40a721c4a94d952854ef",
+    "url": "/fetch-jokes/static/js/2.18cd1dfe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fetch-jokes/static/js/2.18cd1dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff09c25cafa73ebf904d",
+    "url": "/fetch-jokes/static/js/main.6a79a675.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/fetch-jokes/static/js/main.6a79a675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1948ecc3e5dca80ad39",
+    "url": "/fetch-jokes/static/js/runtime-main.20e5b820.js"
+  }
+]);
